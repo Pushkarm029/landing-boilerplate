9f3fde292cc5dc3502b2bb8d8d788bd134818789
Pushkar Mishra <pushkarmishra029@gmail.com>
2024-05-03 23:45:24 +0530

    some ref links
    Signed-off-by: Pushkar Mishra <pushkarmishra029@gmail.com>

--- src/app/page.js
+++ src/app/page.js
@@ -13,3 +13,6 @@ export default function Home() {
 }
 
 // color palette : https://colorhunt.co/palette/ff9843ffdd9586a7fc3468c0
+// https://ui.shadcn.com/docs/installation/next
+// https://mui.com/material-ui/getting-started/installation/
+// https://github.com/AtotheY/saas-landingpage
