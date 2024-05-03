300206f305d657afc4f94968d89d3ea96caf710d
Pushkar Mishra <pushkarmishra029@gmail.com>
2024-05-03 23:43:12 +0530

    basic
    Signed-off-by: Pushkar Mishra <pushkarmishra029@gmail.com>

--- src/app/body.js
+++ src/app/body.js
@@ -1,1 +1,29 @@
 'use client'
+
+import {Container, Typography, Button} from "@mui/material"
+
+export default function Body() {
+  return (
+    <Container sx={{
+      display: 'flex',
+      flexDirection: 'column',
+      justifyContent: 'center',
+      alignItems: 'center',
+      minHeight: '94vh', // Set minimum height to full viewport height
+      padding: 4, // Add padding for content spacing
+      background: 'background.default', // Use default background color
+    }}>
+      <Typography variant="h2" className="h2">
+        Welcome to My Awesome Landing Page
+      </Typography>
+      <Typography variant="body1" paragraph>
+        This is a simple example of a landing page built with Next.js and Material-UI.
+      </Typography>
+      {/* <Link href="/about" passHref> */}
+        <Button variant="contained" color="primary">
+          Learn More
+        </Button>
+      {/* </Link> */}
+    </Container>
+  )
+}
--- src/app/page.js
+++ src/app/page.js
@@ -1,11 +1,15 @@
 'use client';
 
+import Body from "./body";
 import { Navigation } from "./navbar";
 
 export default function Home() {
   return (
     <main>
       <Navigation/>
+      <Body/>
     </main>
   );
 }
+
+// color palette : https://colorhunt.co/palette/ff9843ffdd9586a7fc3468c0
